80e366620e2f63bb92f5bde81094e75cdee3ab6f
Sergio Ocampo <ocamp020@umn.edu>
2015-12-17 06:18:18 -0600

    New Calibration theta=1.5 threshold=0.5

--- programfunctions.f90
+++ programfunctions.f90
@@ -674,11 +674,12 @@ end Subroutine Asset_Grid_Threshold
 		! Endogenous grid for asset income
 		Y_endo = agrid_t(ai) + C_endo - Y_h(H_endo,age,lambdai,ei,wage)
 
+		!$omp critical
 		print*, "EGM Working Periods"
 		print*, C_endo,H_endo,Y_endo
 		print*, MB_in,state_FOC
 		print*, ' '
-
+		!$omp end critical
 	end Subroutine EGM_Working_Period
 
 !========================================================================================
