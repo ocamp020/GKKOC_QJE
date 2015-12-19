e73df0a963259c5cd323696998089c4835f187f0
Sergio Ocampo <ocamp020@umn.edu>
2015-12-19 07:20:19 -0600

    Server theta=2.0 threshold=0

--- parameters.f90
+++ parameters.f90
@@ -41,7 +41,7 @@ MODULE parameters
 		! Depreciation rate
 		REAL(DP), PARAMETER  :: DepRate=0.0_DP
 		! Financial constraints
-		REAL(DP), PARAMETER  :: theta=1.00_dp
+		REAL(DP), PARAMETER  :: theta=2.00_dp
 	
 
 	! Life cycle: retirement age, maximum age
