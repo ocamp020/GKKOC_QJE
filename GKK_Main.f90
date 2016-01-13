664a944fab4f11da8e9ca0ce2203663104bdd7b8
Sergio Ocampo <ocamp020@umn.edu>
2016-01-12 19:06:15 -0600

    Opt Tax theta 1 th 1 K

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -36,9 +36,9 @@ PROGRAM main
 		character(100) :: folder_aux
 
 	! Capital Market
-		theta = 1.50_dp
+		theta = 1.00_dp
 	! Threshold 
-		Threshold_Factor = 4.00_dp 
+		Threshold_Factor = 0.00_dp 
 
 	! Switch for solving benchmark or just reading resutls
 		! If compute_bench==.true. then just read resutls
@@ -47,7 +47,7 @@ PROGRAM main
 			compute_bench = .false.
 			compute_exp   = .false.
 		Opt_Tax       = .true.
-			Opt_Tax_KW    = .false. ! true=tau_K false=tau_W
+			Opt_Tax_KW    = .true. ! true=tau_K false=tau_W
 
 
 	! Switch for separable and non-separable utility
