fc37b88d19c798941d54aa4f8a182c1e1769cf88
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-14 12:22:16 -0500

    revert to before markov_cut

--- programfunctions.f90
+++ programfunctions.f90
@@ -1901,7 +1901,7 @@ SUBROUTINE FIND_DBN_EQ()
 	    	! Solve for new R 
 	    	!R = zbrent(Agg_Debt,0.0_dp,0.50_dp,brent_tol) 
 	    	if (theta .gt. 1.0_DP) then
-	           brent_value = brent(-0.5_DP,0.1_DP,0.99_DP,Agg_Debt, brent_tol,R)
+	           brent_value = brent(-0.1_DP,0.1_DP,0.5_DP,Agg_Debt, brent_tol,R)
             else
                 R = 0.0_DP
 	        endif
@@ -2760,7 +2760,7 @@ SUBROUTINE EGM_RETIREMENT_WORKING_PERIOD()
 	!------Working Period Starts-------------------------------------------------------------
 
 	DO age=RetAge-1,1,-1
-	!$omp parallel do private(lambdai,ei,ai,EndoCons,EndoHours,EndoYgrid,sw,sort_ind,tempai,C_foc,state_FOC,par_FOC)
+	! !$omp parallel do private(lambdai,ei,ai,EndoCons,EndoHours,EndoYgrid,sw,sort_ind,tempai,C_foc,state_FOC,par_FOC)
     DO zi=1,nz
     DO lambdai=1,nlambda
     DO ei=1,ne	
@@ -2798,7 +2798,7 @@ SUBROUTINE EGM_RETIREMENT_WORKING_PERIOD()
 	    ! !$omp critical
 		if (any(isnan(EndoCons))) then 
 			print*, "isnan - Consumption endogenous"
-			print*, age,ai,zi,lambdai,ei
+			print*, age,zi,ai,lambdai,ei
 			print*, EndoCons
 			STOP 
 		end if 
