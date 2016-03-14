a10d26e4f8089b38f4801e2608634072ac01f2ed
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-14 12:06:08 -0500

    revert to before markov_cut

--- programfunctions.f90
+++ programfunctions.f90
@@ -3315,24 +3315,18 @@ SUBROUTINE  INITIALIZE()
 	INTEGER  :: ee0, ee1, ee2, zindx1, zindx2, lambdaindx1, lambdaindx2, diff_array, eindx1, eindx2
 	INTEGER, DIMENSION(RetAge) :: agevec
 	! Entrepreneurial ability
-		! grid (zgrid), invariant distribution (Glz)
-		REAL(DP), DIMENSION(nz_aux)    :: zgrid_aux , Gz_aux
-		! transition matrix (pr_z)
-		REAL(DP), DIMENSION(nz_aux,nz_aux) :: pr_z_aux
+		! grid (zgrid), invariant distribution (Glz), CDF of invariant distribution (cdf_Gz)
+		REAL(DP), DIMENSION(nz_aux)    :: zgrid_aux , Gz_aux, cdf_Gz_aux
+		! transition matrix (pr_z), CDF of transition matrix (by row) (cdf_pr_z)
+		REAL(DP), DIMENSION(nz_aux,nz_aux) :: pr_z_aux, cdf_pr_z_aux
 	
-	! Initiliaze grids for z
-		CALL tauchen(mtauchen_z,rho_z,sigma_z_eps,nz_aux,zgrid_aux,pr_z_aux,Gz_aux)
-		zgrid = zgrid_aux
-		pr_z = pr_z_aux
-		Gz   = Gz_aux
-		! Tauchen gives grids for the log of the variables. Exponentiate to adjust
-		zgrid_aux      = exp(zgrid_aux) + mu_z
-		! Cut process 
-		!CALL Markov_Cut(nz_aux,zgrid_aux,pr_z_aux,Gz_aux,nz_aux-nz,zgrid,pr_z,Gz)
-
-	! Initiliaze grids for lamda and e
+	! Initiliaze grids for z, lamda and e	
+		CALL tauchen(mtauchen_z,rho_z,sigma_z_eps,nz,zgrid,pr_z,Gz)
 		CALL tauchen(mtauchen,rho_E,sigma_e_eps,ne,egrid,pr_e,Ge)
 		CALL tauchen(mtauchen,rho_lambda,sigma_lambda_eps,nlambda,lambdagrid,pr_lambda,Glambda)
+
+		! Tauchen gives grids for the log of the variables. Exponentiate to adjust
+		zgrid      = exp(zgrid) + mu_z
 		egrid      = exp(egrid)
 		lambdagrid = exp(lambdagrid) 
 
