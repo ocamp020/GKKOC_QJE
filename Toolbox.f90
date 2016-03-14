305869f81c17b11dcdee9c9bd41a4e00790ad386
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-14 15:22:29 -0500

    Test Markov_Cut

--- Toolbox.f90
+++ Toolbox.f90
@@ -778,6 +778,9 @@ MODULE Toolbox
 			G(1)  = sum(G_in(1:n_cut+1))
 			G(2:) = G_in(n_cut+2:)
 			print*, 'Test!!!!!!!!!!!!!!!!!!!!!'
+			print*, G_cut
+			print*, sum(G_cut)
+			print*, "End Test"
 		endif
 
 	end Subroutine Markov_Cut
