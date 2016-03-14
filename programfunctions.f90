3dd41cf8eae02767044c5e93b104ab6f688f149b
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-14 14:52:56 -0500

    Test Markov_Cut

--- programfunctions.f90
+++ programfunctions.f90
@@ -3332,6 +3332,9 @@ SUBROUTINE  INITIALIZE()
 
 		! Cut bottom elements of zgrid 
 		CALL Markov_Cut(nz_aux,zgrid_aux,pr_z_aux,Gz_aux,nz_aux-nz,zgrid,pr_z,Gz)
+		if (Gz.eq.Gz_aux) then print*, 'Gz check' endif 
+		if (zgrid.eq.zgrid_aux) then print*, 'zgrid check' endif 
+		if (pr_z.eq.pr_z_aux) then print*, 'pr_z check' endif 
 
 		! Obtain CDF of invariant distribution and transition matrix
 			! Entrepreneurial ability
