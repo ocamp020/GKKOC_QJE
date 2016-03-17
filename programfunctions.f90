381e0545b4816022c289e043e5de356cd2220648
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-17 06:41:06 -0500

    Getting ready code for sending to Burhan

--- programfunctions.f90
+++ programfunctions.f90
@@ -4284,7 +4284,7 @@ SUBROUTINE  SIMULATION(bench_indx)
 		!     	where(panelage==1) eligible = 0 
 	 !        endif 
 
-	 		print*, "Simulation period", simutime
+	 		! print*, "Simulation period", simutime
 		ENDDO ! simutime
 
 		! ! Clean eligibles 
