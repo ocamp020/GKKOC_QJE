5080860d62f6008f02592937a7a2d5461a2bc915
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-20 18:29:16 -0500

    Testing intergenerational update

--- programfunctions.f90
+++ programfunctions.f90
@@ -4069,13 +4069,12 @@ SUBROUTINE  SIMULATION(bench_indx)
 	REAL(DP), DIMENSION(totpop) 			  :: panela_new_1  , panela_new_2  , panela_new_3 
 	REAL(DP), DIMENSION(:)      , allocatable :: eligible_panela_old_1  , eligible_panela_old_2  , eligible_panela_old_3
 	REAL(DP), DIMENSION(:)      , allocatable :: eligible_panela_new_1  , eligible_panela_new_2  , eligible_panela_new_3
-	INTEGER , DIMENSION(totpop) 			  :: age_old_1, panelage_old_2, panelage_old_3
-	INTEGER , DIMENSION(totpop) :: panelage_1
+	INTEGER , DIMENSION(:)      , allocatable :: panelage_old_1, panelage_old_2, panelage_old_3
 	INTEGER , DIMENSION(totpop) 			  :: panelage_new_1, panelage_new_2, panelage_new_3 
 	INTEGER , DIMENSION(:)      , allocatable :: eligible_panelage_old_1, eligible_panelage_old_2, eligible_panelage_old_3
 	INTEGER , DIMENSION(:)      , allocatable :: eligible_panelage_new_1, eligible_panelage_new_2, eligible_panelage_new_3
 	INTEGER                     			  :: n_eligible
-
+	allocate(panelage_old_1(totpop),panelage_old_2(totpop),panelage_old_3(totpop))
 
 	!$ call omp_set_num_threads(20)
 
@@ -4312,7 +4311,7 @@ SUBROUTINE  SIMULATION(bench_indx)
 			allocate( eligible_panelage_old_1(n_eligible), eligible_panelage_old_2(n_eligible), eligible_panelage_old_3(n_eligible) )
 			allocate( eligible_panelage_new_1(n_eligible), eligible_panelage_new_2(n_eligible), eligible_panelage_new_3(n_eligible) )
 
-			eligible_panelage_old_1 = pack(panelage_1 , (eligible.eq.1) )
+			! eligible_panelage_old_1 = pack(panelage_old_1 , (eligible.eq.1) )
 			! eligible_panelage_old_2 = pack(panelage_old_2 , (eligible.eq.1) )
 			! eligible_panelage_old_3 = pack(panelage_old_3 , (eligible.eq.1) )
 			! eligible_panelage_new_1 = pack(panelage_new_1 , (eligible.eq.1) )
