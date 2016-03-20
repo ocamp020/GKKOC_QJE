44094b1e7770cf0de48f647f126116f855324708
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-20 12:13:34 -0500

    Testing intergenerational update

--- programfunctions.f90
+++ programfunctions.f90
@@ -4301,7 +4301,7 @@ SUBROUTINE  SIMULATION(bench_indx)
 			allocate( eligible_panelage_old_1(n_eligible), eligible_panelage_old_2(n_eligible), eligible_panelage_old_3(n_eligible) )
 			allocate( eligible_panelage_new_1(n_eligible), eligible_panelage_new_2(n_eligible), eligible_panelage_new_3(n_eligible) )
 
-		! 	eligible_panela_old_1 = pack(panela_old_1 , (eligible.eq.1) )
+			eligible_panela_old_1 = pack(panela_old_1 , (eligible.eq.1) )
 		! 	eligible_panela_old_2 = pack(panela_old_2 , (eligible.eq.1) )
 		! 	eligible_panela_old_3 = pack(panela_old_3 , (eligible.eq.1) )
 		! 	eligible_panela_new_1 = pack(panela_new_1 , (eligible.eq.1) )
