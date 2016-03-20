3f905955d4c8225339d7b6dfd6039d11991c8721
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-20 13:08:19 -0500

    Testing intergenerational update

--- programfunctions.f90
+++ programfunctions.f90
@@ -4064,14 +4064,16 @@ SUBROUTINE  SIMULATION(bench_indx)
 	INTEGER , DIMENSION(totpop) :: panelage, panelz, panellambda, panele, panelx
 	REAL(DP), DIMENSION(totpop) :: panela, panelPV_a, panelK  
 	! Intergenerational statistics
-	INTEGER ,  DIMENSION(totpop) :: eligible, death_count
-	REAL(DP), DIMENSION(totpop) :: panela_old_1  , panela_old_2  , panela_old_3  , panela_new_1  , panela_new_2  , panela_new_3 
-	INTEGER , DIMENSION(totpop) :: panelage_old_1, panelage_old_2, panelage_old_3, panelage_new_1, panelage_new_2, panelage_new_3 
-	Real(DP), allocatable       :: eligible_panela_old_1(:)  , eligible_panela_old_2(:)  , eligible_panela_old_3(:)
-	Real(DP), allocatable       :: eligible_panela_new_1(:)  , eligible_panela_new_2(:)  , eligible_panela_new_3(:)
-	INTEGER , allocatable       :: eligible_panelage_old_1(:), eligible_panelage_old_2(:), eligible_panelage_old_3(:)
-	INTEGER , allocatable       :: eligible_panelage_new_1(:), eligible_panelage_new_2(:), eligible_panelage_new_3(:)
-	INTEGER                     :: n_eligible
+	INTEGER , DIMENSION(totpop) 			  :: eligible, death_count
+	REAL(DP), DIMENSION(totpop) 			  :: panela_old_1  , panela_old_2  , panela_old_3
+	REAL(DP), DIMENSION(totpop) 			  :: panela_new_1  , panela_new_2  , panela_new_3 
+	REAL(DP), DIMENSION(:)      , allocatable :: eligible_panela_old_1(:)  , eligible_panela_old_2(:)  , eligible_panela_old_3(:)
+	REAL(DP), DIMENSION(:)      , allocatable :: eligible_panela_new_1(:)  , eligible_panela_new_2(:)  , eligible_panela_new_3(:)
+	INTEGER , DIMENSION(totpop) 			  :: panelage_old_1, panelage_old_2, panelage_old_3
+	INTEGER , DIMENSION(totpop) 			  :: panelage_new_1, panelage_new_2, panelage_new_3 
+	INTEGER , DIMENSION(:)      , allocatable :: eligible_panelage_old_1(:), eligible_panelage_old_2(:), eligible_panelage_old_3(:)
+	INTEGER , DIMENSION(:)      , allocatable :: eligible_panelage_new_1(:), eligible_panelage_new_2(:), eligible_panelage_new_3(:)
+	INTEGER                     			  :: n_eligible
 
 
 	!$ call omp_set_num_threads(20)
