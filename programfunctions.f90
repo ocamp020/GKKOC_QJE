cba3adb384c41863cc11b3753740dc4ffee785e2
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-20 17:44:58 -0500

    Testing intergenerational update

--- programfunctions.f90
+++ programfunctions.f90
@@ -4316,7 +4316,7 @@ SUBROUTINE  SIMULATION(bench_indx)
 			! eligible_panelage_new_1 = pack(panelage_new_1 , (eligible.eq.1) )
 			! eligible_panelage_new_2 = pack(panelage_new_2 , (eligible.eq.1) )
 			! eligible_panelage_new_3 = pack(panelage_new_3 , (eligible.eq.1) )
-			eligible_panelage_old_1 	= pack(panelage_old_1   , (eligible.eq.1) )
+			eligible_panela_old_1 	= pack(panelage_old_1   , (eligible.eq.1) )
 
 		print*, ' '
 		print*, 'n_eligible', sum(eligible)
