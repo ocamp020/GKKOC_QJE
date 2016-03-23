0068196d8112a10d409a1c2fdbe144f97d3e31e8
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-23 08:58:29 -0500

    Test x_hi=3.0

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -88,7 +88,7 @@ PROGRAM main
 		sigma  	= 4.0_dp
 		phi    	= (1.0_dp-gamma)/gamma
 
-		x_hi	= 2.80_dp
+		x_hi	= 1.50_dp
 		x_lo	= 1.0_dp
 		a_x 	= 0.10_dp
 		b_x 	= 0.40_dp
@@ -131,7 +131,7 @@ PROGRAM main
 			Result_Folder = './SU_ZS_PT_Results/Theta_'//trim(string_theta)//'/Factor_'//trim(Result_Folder)//'/'
 		end if
 
-		Result_Folder = trim(Result_Folder)//'x_hi_28_zeps_130_z11_9/' 
+		Result_Folder = trim(Result_Folder)//'prod_shock/' 
 
 		! call execute_command_line( 'mkdir -p ' // trim(Result_Folder) )
 		call system( 'mkdir -p ' // trim(Result_Folder) )
