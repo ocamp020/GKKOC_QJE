a928ddb338ba61ffd45b306f54e343c7d30be461
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-23 10:57:17 -0500

    Added files for calibration loop

--- GKK_Calibration_Loop.f90
+++ GKK_Calibration_Loop.f90
@@ -92,7 +92,7 @@ PROGRAM GKK_Calibration_Loop
 
 	! Set Parameters from Loop
 		OPEN (UNIT=3, FILE=trim(Result_Folder)//'Loop_Par'  , STATUS='old', ACTION='read')
-		WRITE(unit=3, FMT=*) Loop_Par
+		READ (UNIT=3,  FMT=*), Loop_Par
 		CLOSE(unit=3)
 
 		beta 		= Loop_Par(1)
