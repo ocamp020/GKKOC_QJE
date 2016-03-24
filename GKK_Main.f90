4c7a659ed8cd855c265043865a3ab3e43fcadbb0
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-24 13:23:35 -0500

    Added files for calibration loop

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -52,7 +52,7 @@ PROGRAM main
 		! If compute_bench==.true. then just read resutls
 		! If compute_bench==.false. then solve for benchmark and store results
 		Tax_Reform    = .true.
-			compute_bench = .true.
+			compute_bench = .false.
 			compute_exp   = .true.
 		Opt_Tax       = .false.
 			Opt_Tax_KW    = .false. ! true=tau_K false=tau_W
