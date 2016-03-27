d3dfea869528e1c290fb56f8f69c59ae8c5381a3
Sergio Ocampo Diaz <ocamp020@github.com>
2016-03-27 14:20:18 -0500

    Added files for calibration loop

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -94,12 +94,12 @@ PROGRAM main
 		! 0.957 0.530   5       3.0320955	0.80911831  	0.39959548	8.193043463	 35.30278	61.27262    0.1     0.0 	0.465
 
 
-		beta   	= 0.96_dp  ! params(1)
+		beta   	= 0.95_dp! 0.96_dp  ! params(1)
 		mu_z   	= params(2) ! this is just shifting the z grids. it is zero now.
 		rho_z  	= params(3) 
-		sigma_z_eps      = 0.112_dp ! 0.33_dp ! 0.07_dp ! ! params(4)
+		sigma_z_eps      = 0.07_dp ! 0.33_dp ! 0.07_dp ! ! params(4)
 		sigma_lambda_eps = params(5)
-		gamma  	= 0.455_dp ! params(6) ! 0.455_dp !
+		gamma  	=  params(6) ! 0.455_dp !
 		
 		sigma  	= 4.0_dp
 		phi    	= (1.0_dp-gamma)/gamma
