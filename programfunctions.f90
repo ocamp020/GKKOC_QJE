10e7ee07058aec3e699f3ea4aa27ec848d43985c
Sergio Ocampo Diaz <ocamp020@github.com>
2016-04-03 12:24:05 -0400

    Simulation with labor income and PV mu80 low x

--- programfunctions.f90
+++ programfunctions.f90
@@ -4739,7 +4739,7 @@ SUBROUTINE  SIMULATION_TOP(bench_indx)
      			& (mu*P*xz_grid(panelx_top(age_top,ii),panelz_top(age_top,ii))**mu & 
      				& /(R+DepRate))**(1.0_dp/(1.0_dp-mu)) )
 
-     		panel_PV_top(paneli) = (   (agrid(tkhi) - panela_top(age_top,ii)) * V_Pr(panelage_top(age_top,ii),tklo,&
+     		panel_PV_top(age_top,ii) = (   (agrid(tkhi) - panela_top(age_top,ii)) * V_Pr(panelage_top(age_top,ii),tklo,&
 				        	&	 panelz_top(age_top,ii),panel_lambda_top(age_top,ii),panele_top(age_top,ii),panelx_top(age_top,ii)) &
 			                &  +    (panela_top(age_top,ii) - agrid(tklo)) * V_Pr(panelage_top(age_top,ii),tkhi,&
 		        			&	 panelz_top(age_top,ii),panel_lambda_top(age_top,ii),panele_top(age_top,ii),panelx_top(age_top,ii)) )&
