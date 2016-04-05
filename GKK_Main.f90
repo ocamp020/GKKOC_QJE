3e4823b9eea31ee1c42b5edf855ce89dc92b9742
Sergio Ocampo Diaz <ocamp020@github.com>
2016-04-05 07:20:52 -0400

    Test Exp Shock mu90 top simulation

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -56,10 +56,10 @@ PROGRAM main
 		! If compute_bench==.false. then solve for benchmark and store results
 		Tax_Reform    = .true.
 			compute_bench = .false.
-			compute_exp   = .true.
+			compute_exp   = .false.
 		Opt_Tax       = .false.
 			Opt_Tax_KW    = .false. ! true=tau_K false=tau_W
-		Simul_Switch  = .true.
+		Simul_Switch  = .false.
 
 
 	! Switch for separable and non-separable utility
@@ -283,7 +283,7 @@ Subroutine Solve_Benchmark(compute_bench,Simul_Switch)
 			print*,"	Simulation"
 			CALL SIMULATION(solving_bench)
 		endif
-		!!CALL SIMULATION_TOP(solving_bench)
+		CALL SIMULATION_TOP(solving_bench)
 
 	! Aggregate variables in benchmark economy
 		GBAR_bench  = GBAR
