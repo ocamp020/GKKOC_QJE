6265c07ae0c2ea7cdaabc3904b969c34ba694b26
Sergio Ocampo Diaz <ocamp020@github.com>
2016-04-26 10:06:27 -0400

    Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -81,15 +81,6 @@ PROGRAM main
 		Params =[0.962_dp, 0.0_dp, 0.50_dp, 0.387_dp, 0.29_dp, 0.4494_dp] ! alpha=0.4, zgrid 11, m5, alpha=0.4, dep005, mu=090, K/Y=3, Top1PVa=0.36
 		! Params =[0.973_dp, 0.0_dp, 0.50_dp, 0.6567_dp,  0.30_dp,  0.4494_dp] ! mu=0.8
 		
-		! Multiplicative Shock
-		! beta	sigmaz	x_hi	W/GDP		STD_Earnings	Mean_Labor	MeanReturn	PV_Top_1%	PV_Top_10%	a_x 	b_x
-		! 0.95	0.25	3		3.079403947	0.789919246		0.387819464	7.99E-02	36.60252401	71.46784902	0.1 	0.4
-		! Retirement Shock mu=0.9, matuchenz=5, nz_aux=11, nz=9
-		! beta	sigmaz	x_hi	W/GDP		STD_Earnings	Mean_Labor	MeanReturn	PV_Top_1%	PV_Top_10%	a_x 	b_x 	gamma 
-		! 0.953 0.340   5       3.05129691	0.801885794 	0.3965338	8.11022612	 35.79579	61.4643909  0.1     0.0 	0.457
-		! Retirement Shock mu=0.8, matuchenz=5, nz_aux=11, nz=9
-		! beta	sigmaz	x_hi	W/GDP		STD_Earnings	Mean_Labor	MeanReturn	PV_Top_1%	PV_Top_10%	a_x 	b_x 	gamma 
-		! 0.957 0.530   5       3.0320955	0.80911831  	0.39959548	8.193043463	 35.30278	61.27262    0.1     0.0 	0.465
 		! Exponential shock mu=0.9, matuchenz=5, nz_aux=11, nz=9
 		! beta	sigmaz	x_hi	W/GDP		STD_Earnings	Mean_Labor	MeanReturn	PV_Top_1%	PV_Top_10%	a_x 	b_x
 		! 0.95	0.07	5		2.952165	0.79992289		0.38810872	8.55075051	35.073503	61.033801	0.1 	0.0
@@ -106,10 +97,10 @@ PROGRAM main
 		! beta 		sigmaz 		x_hi 	rho_z 	gamma
 		! 0.954_dp 0.074_dp  	5.00_dp 0.1_dp 	0.457_dp
 
-		beta   	= 0.954_dp ! params(1) !
+		beta   	= 0.950_dp ! params(1) !
 		mu_z   	= params(2) ! this is just shifting the z grids. it is zero now.
 		rho_z  	= 0.1_dp ! params(3) 
-		sigma_z_eps      =  0.074_dp ! params(4) ! 0.01_dp ! ! 
+		sigma_z_eps      =  0.09_dp ! params(4) ! 0.01_dp ! ! 
 		sigma_lambda_eps = params(5)
 		gamma  	=  0.457_dp !  0.465_dp ! params(6) ! 
 		
