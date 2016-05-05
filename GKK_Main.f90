6a4557aa03f0012185a18da32b1fe511a8a8d191
Sergio Ocampo Diaz <ocamp020@github.com>
2016-05-05 16:22:39 -0400

    initial commit model 1.2

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -90,9 +90,9 @@ PROGRAM main
 		beta   	= 0.948_dp ! params(1) !
 		mu_z   	= params(2) ! this is just shifting the z grids. it is zero now.
 		rho_z  	= 0.1_dp ! params(3) 
-		sigma_z_eps      =  0.0585_dp ! params(4) ! 0.01_dp ! ! 
+		sigma_z_eps      =  0.077_dp ! params(4) ! 0.01_dp ! ! 
 		sigma_lambda_eps = params(5)
-		gamma  	=  0.462_dp !  0.465_dp ! params(6) ! 
+		gamma  	=  0.457_dp !  0.465_dp ! params(6) ! 
 		Params =[beta, mu_z, rho_z, sigma_z_eps, sigma_lambda_eps, gamma] 
 		
 		sigma  	= 4.0_dp
@@ -143,7 +143,7 @@ PROGRAM main
 		end if
 
 		! Result_Folder = trim(Result_Folder)//'Ret_Shock/' 
-		Result_Folder = trim(Result_Folder)//'Ret_Shock_theta10/' 
+		Result_Folder = trim(Result_Folder)//'Model_1.2/' 
 
 		! call execute_command_line( 'mkdir -p ' // trim(Result_Folder) )
 		call system( 'mkdir -p ' // trim(Result_Folder) )
