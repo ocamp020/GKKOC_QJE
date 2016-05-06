565496f05c862ef367654c2a8a157f80775fe180
Sergio Ocampo Diaz <ocamp020@github.com>
2016-05-06 11:15:48 -0400

    IGM updated

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -59,7 +59,7 @@ PROGRAM main
 			compute_exp   = .false.
 		Opt_Tax       = .false.
 			Opt_Tax_KW    = .false. ! true=tau_K false=tau_W
-		Simul_Switch  = .true.
+		Simul_Switch  = .false.
 
 
 	! Switch for separable and non-separable utility
