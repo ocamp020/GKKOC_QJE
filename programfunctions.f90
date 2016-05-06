8a9c729761ac41a5c5478664d6d3f6f72e7bace8
Sergio Ocampo Diaz <ocamp020@github.com>
2016-05-06 07:52:15 -0400

    IGM updated

--- programfunctions.f90
+++ programfunctions.f90
@@ -4404,7 +4404,7 @@ SUBROUTINE  SIMULATION(bench_indx)
 		     	endif 
 		     	! Reset variables if son dies before 60
 		     	if ((age.eq.1).and.(age_son_2(paneli).lt.41)) then 
-		     		! age_dad_2(paneli)    = 0 	  ; age_son_2(paneli)    = 0 
+		     		age_dad_2(paneli)    = 0 	  ; age_son_2(paneli)    = 0 
 		     		! assets_dad_2(paneli) = 0.0_dp ; assets_son_2(paneli) = 0.0_dp
 		     	endif 
 		     	! ! Save results 
