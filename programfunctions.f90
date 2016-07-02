b24db344322e67d74487aef00382413f7275dd38
Sergio Ocampo Diaz <ocamp020@github.com>
2016-07-02 15:01:46 -0500

    Simulation and Tax Reform

--- programfunctions.f90
+++ programfunctions.f90
@@ -4595,7 +4595,10 @@ SUBROUTINE  SIMULATION(bench_indx)
 		    	where(panelage==1) eligible = 0 
 	        endif 
 
-	 		print*, "Simulation period", simutime
+	 		
+	 		! print*, "Simulation period", simutime
+
+	 		
 		ENDDO ! simutime
 		print*,' '
 		print*,'Averages'
