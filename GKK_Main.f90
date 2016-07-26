1360a86bf2451cae66fe2461a964595c89de5ffe
Sergio Ocampo Diaz <ocamp020@github.com>
2016-07-26 19:22:23 -0400

    Calibration x_hi=10

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -87,7 +87,7 @@ PROGRAM main
 		! Calibration to book value and rho_z=0.1 and x_hi=10
 
 
-		beta   	= 0.945_dp! 0.95_dp ! params(1) !
+		beta   	= 0.947_dp! 0.95_dp ! params(1) !
 		mu_z   	= params(2) ! this is just shifting the z grids. it is zero now.
 		rho_z  	= 0.1_dp ! params(3) 
 		sigma_z_eps      =  0.037_dp !0.115_dp ! params(4) ! 0.01_dp ! ! 
@@ -251,6 +251,7 @@ Subroutine Solve_Benchmark(compute_bench,Simul_Switch)
 	! Solve for the model and compute stats
 	print*,"	Initializing program"
 		CALL INITIALIZE
+		CALL Write_Benchmark_Results(compute_bench)
 	if (compute_bench) then
 		print*,"	Computing equilibrium distribution"
 		CALL FIND_DBN_EQ
