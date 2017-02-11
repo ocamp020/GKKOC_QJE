6ecf384f87da00a1c4430d12084420bcf07e4575
Sergio Ocampo <ocamp020@github.com>
2017-02-11 17:44:27 -0600

    Optimal Consumption taxes 50%-100% - Coarse grid

--- analysis.sh
+++ analysis.sh
@@ -2,5 +2,5 @@
 module load gcc/5.2
 cd RA_Guvenen/GKKC_Code/
 make clean
-rm -f log_analysis_sh_opt_tauK.txt
-make GKK_Main > log_analysis_sh_opt_tauK.txt 
+rm -f log_analysis_sh_opt_tauK_tauC_coarse_grid.txt
+make GKK_Main > log_analysis_sh_opt_tauK_tauC_grid.txt 
