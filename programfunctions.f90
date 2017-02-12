0f47da529aadc9f960ab4ffbf8fc5c0b4c8d47f8
Sergio Ocampo <ocamp020@github.com>
2017-02-12 12:20:46 -0600

    Hsieh and Klenow without distortions

--- programfunctions.f90
+++ programfunctions.f90
@@ -3760,9 +3760,7 @@ Function Agg_Debt(R_in)
 	enddo 
 	enddo 
 	enddo 
-	do xi=1,nx 
-		print*, (mu*P*xz_grid(xi,:)**mu/(R_in+DepRate))**(1.0_dp/(1.0_dp-mu))
-	enddo 
+	print*, (mu*P*xz_grid(1,5:)**mu/(R_in+DepRate))**(1.0_dp/(1.0_dp-mu))
 	print*, '------------',Wealth, Kd, Agg_Debt, R_in, P
 	Agg_Debt = (Agg_Debt/Wealth)**2.0_dp
 
