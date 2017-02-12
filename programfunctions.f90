a0843ad4a12d95b2c308d030529d6683cfe7e39c
Sergio Ocampo <ocamp020@github.com>
2017-02-12 11:25:58 -0600

    Hsieh and Klenow without distortions

--- programfunctions.f90
+++ programfunctions.f90
@@ -1925,8 +1925,8 @@ SUBROUTINE FIND_DBN_EQ()
 	    	! Solve for new R 
 	    	!R = zbrent(Agg_Debt,0.0_dp,0.50_dp,brent_tol) 
 	    	if (sum(theta)/nz .gt. 1.0_DP) then
-	    		P = min(P,1.0_dp)
-	           brent_value = brent(-0.1_DP,0.1_DP,20.0_DP,Agg_Debt, brent_tol,R)
+	    		P = min(P,0.5_dp)
+	           brent_value = brent(-0.1_DP,0.1_DP,10.0_DP,Agg_Debt, brent_tol,R)
             else
                 R = 0.0_DP
 	        endif
