d72702422696d5ec426baef7d020570a47e75f3f
Sergio Ocampo <ocamp020@github.com>
2017-02-17 15:37:45 -0600

    Optimal consumption taxes holding labor taxes fixed

--- analysis.sh
+++ analysis.sh
@@ -2,5 +2,5 @@
 module load gcc/5.2
 cd RA_Guvenen/GKKC_Code/
 make clean
-rm -f log_analysis_sh_opt_tauK_tauC_coarse_grid.txt
-make GKK_Main > log_analysis_sh_opt_tauK_tauC_grid.txt 
+rm -f log_analysis_sh_opt_tauK_tau_W_CX.txt
+make GKK_Main > log_analysis_sh_opt_tau_W_CX.txt 
