f58661b62cd96b32dcb0b3e99dd04d4200cf235f
Sergio Ocampo <ocamp020@github.com>
2017-03-22 11:55:38 -0500

    Simulation of Life Cycle Patterns for benchmark and tax reform

--- Simulation_Module.f90
+++ Simulation_Module.f90
@@ -1874,12 +1874,12 @@ SUBROUTINE  Simulation_Life_Cycle_Patterns(bench_indx)
 	DO age = 1,MaxAge 
 		WRITE  (UNIT=10, FMT='(F12.4)') Mean_a(age,:)
 		WRITE  (UNIT=11, FMT='(F12.4)') Mean_c(age,:)
-		WRITE  (UNIT=10, FMT='(F12.4)') Mean_k(age,:) 
-		WRITE  (UNIT=11, FMT='(F12.4)') Mean_h(age,:)
-		WRITE  (UNIT=10, FMT='(F12.4)') Mean_r(age,:)
-		WRITE  (UNIT=11, FMT='(F12.4)') Mean_r_at(age,:)
-		WRITE  (UNIT=10, FMT='(F12.4)') Mean_r_w(age,:)
-		WRITE  (UNIT=11, FMT='(F12.4)') Mean_r_at_w(age,:) 
+		WRITE  (UNIT=12, FMT='(F12.4)') Mean_k(age,:) 
+		WRITE  (UNIT=13, FMT='(F12.4)') Mean_h(age,:)
+		WRITE  (UNIT=14, FMT='(F12.4)') Mean_r(age,:)
+		WRITE  (UNIT=15, FMT='(F12.4)') Mean_r_at(age,:)
+		WRITE  (UNIT=16, FMT='(F12.4)') Mean_r_w(age,:)
+		WRITE  (UNIT=17, FMT='(F12.4)') Mean_r_at_w(age,:) 
 	ENDDO
 
 	close (unit=10); close (unit=11); close (unit=12); close (unit=13); 
