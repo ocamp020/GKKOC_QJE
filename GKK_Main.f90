28353699e365fe070710412a9ee2e814640ac2c8
Sergio Ocampo <ocamp020@github.com>
2017-07-12 09:41:23 -0500

    Optimal Tax Reform

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -670,10 +670,10 @@ Subroutine Find_Capital_and_Wealth_Tax(compute_exp,Simul_Switch)
 	CALL Write_Experimental_Results(.false.)
 
 	! Grid for tauK 
-	OPEN (UNIT=77, FILE=trim(Result_Folder)//'Stats_by_tau_k_4.txt', STATUS='replace')
+	OPEN (UNIT=77, FILE=trim(Result_Folder)//'Stats_by_tau_k_5.txt', STATUS='replace')
 	CLOSE (unit=77) 
 	max_CE2_NB = -10000.0_dp 
-	do tauindx=-60,-80,-2
+	do tauindx=-80,-100,-2
     	tauK = real(tauindx,8)/100_DP
     	print*, 'Capital Tax Grid: tauK=',tauK
     	CE2_NB = Tax_Reform_Welfare(tauK)
@@ -683,7 +683,7 @@ Subroutine Find_Capital_and_Wealth_Tax(compute_exp,Simul_Switch)
 			Opt_tauW = tauW_at 
 		endif
 
-		OPEN (UNIT=77, FILE=trim(Result_Folder)//'Stats_by_tau_k_4.txt', STATUS='old', POSITION='append')
+		OPEN (UNIT=77, FILE=trim(Result_Folder)//'Stats_by_tau_k_5.txt', STATUS='old', POSITION='append')
 		WRITE  (UNIT=77, FMT=*) tauK, tauW_at, psi, GBAR_K/(GBAR_bench +SSC_Payments_bench ), & 
 			      &  MeanWealth, QBAR,NBAR, YBAR, 100.0_DP*(Y_exp/Y_bench-1.0), &
 			      &  wage, sum(ValueFunction(1,:,:,:,:,:)*DBN1(1,:,:,:,:,:))/sum(DBN1(1,:,:,:,:,:)), &
