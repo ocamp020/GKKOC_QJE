8133e641887c5760dcc74ffb5047ef54c6370d3a
Sergio Ocampo <ocamp020@github.com>
2019-02-12 19:55:21 -0600

    Fix bug in CE Decomposition of CE2-Pop

--- Consumption_Equivalent.f90
+++ Consumption_Equivalent.f90
@@ -234,9 +234,9 @@ do i=1,3
 	PRINT*,''
 
 	!CE_total = 100.0_dp*((ValueFunction_exp/ValueFunction_bench)**(1.0_dp/((1.0_dp-sigma)*gamma)) - 1.0_dp ) ;
-	print*, shape(ValueFunction_exp)
-	print*, shape(ValueFunction_bench)
-	print*, shape(CE_total)
+	!print*, shape(ValueFunction_exp)
+	!print*, shape(ValueFunction_bench)
+	!print*, shape(CE_total)
 		print*, ' Test 4.1'
 		
 	! CE2_nb_total = 100.0_dp*&
