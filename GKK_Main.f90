10a692c6df6fd71ac8a9367363e97df1f9db4923
Sergio Ocampo <ocamp020@github.com>
2019-02-26 13:38:47 -0600

    Ready to test tax reform

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -61,9 +61,9 @@ PROGRAM main
 		Calibration_Switch = .false.
 		! If compute_bench==.true. then just read resutls
 		! If compute_bench==.false. then solve for benchmark and store results
-		Tax_Reform    = .false.
+		Tax_Reform    = .true.
 			compute_bench = .false.
-			compute_exp   = .false.
+			compute_exp   = .true.
 			compute_exp_pf= .false.
 				Fixed_PF        = .false.
 				Fixed_PF_interp = .false.
