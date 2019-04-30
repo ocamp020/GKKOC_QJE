4ce85d50ec8edded6b14c3b2c18d4dd73b477d07
Sergio Ocampo <ocamp020@github.com>
2019-04-29 19:17:14 -0500

    initial commit with new functions for transtition

--- analysis.sh
+++ analysis.sh
@@ -2,5 +2,5 @@
 module load gcc/5.2
 cd RA_Guvenen/GKKC_Code/
 make clean
-rm -f log_analysis_sh_opt_tax_threshold_2.txt
-make GKK_Main > log_analysis_sh_opt_tax_threshold_2.txt
+rm -f log_Transition.txt
+make GKK_Main > log_Transition.txt
