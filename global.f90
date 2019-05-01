d2ec02b2fe7f0eff0ab3edb2d36de7c08c84d333
Sergio Ocampo <ocamp020@github.com>
2019-05-01 11:00:13 -0500

    initial commit with new functions for transtition

--- global.f90
+++ global.f90
@@ -166,6 +166,8 @@ MODULE global
     INTEGER :: ti 
     ! Prices and Quantities
     REAL(DP), DIMENSION(T+1) :: R_tr, P_tr, QBAR_tr, NBAR_tr, YBAR_tr, Wage_tr, EBAR_tr
+    ! Government Budget
+    REAL(DP), DIMENSION(T+1) :: GBAR_tr, GBAR_K_tr, GBAR_W_tr, GBAR_L_tr, GBAR_C_tr, SSC_Payments_tr, Tot_Lab_Inc_tr
     ! Policy function and value function (defined on the exogenous grid)
     REAL(DP), DIMENSION(:,:,:,:,:,:,:), allocatable :: Cons_tr, Hours_tr, Aprime_tr, ValueFunction_tr, Cons_Eq_Welfare_tr, DBN_tr
 	! Policy function and value function (defined on the adjusted grid for breakpoints)
