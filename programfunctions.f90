7de4fd9d3ed1e4b962b631c232f4be502daf258b
Sergio Ocampo <ocamp020@github.com>
2019-05-07 16:01:55 -0500

    Experiment with tk-tk convergence

--- programfunctions.f90
+++ programfunctions.f90
@@ -5192,6 +5192,7 @@ SUBROUTINE FIND_DBN_Transition()
         wage_tr = (1.0_DP-alpha)*QBAR_tr**alpha * NBAR_tr**(-alpha)
         Ebar_tr = wage_tr  * NBAR_tr  * sum(pop)/sum(pop(1:RetAge-1))
 
+        print*, trim(Result_Folder)//'Transition_NBAR.txt'
         ! Save initial guess of prices
         OPEN (UNIT=77, FILE=trim(Result_Folder)//'Transition_NBAR.txt', STATUS='replace')
         OPEN (UNIT=78, FILE=trim(Result_Folder)//'Transition_QBAR.txt', STATUS='replace')
