e2576079daa15646216af1e7e10b0315db6feae7
Sergio Ocampo <ocamp020@github.com>
2019-05-13 15:56:00 -0500

    Policy Function Experiment with Exp Prices

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -62,7 +62,7 @@ PROGRAM main
 		Calibration_Switch = .false.
 		! If compute_bench==.true. then just read resutls
 		! If compute_bench==.false. then solve for benchmark and store results
-		Tax_Reform    = .false.
+		Tax_Reform    = .true.
 			compute_bench = .false.
 			compute_exp   = .false.
 			compute_exp_pf= .true.
