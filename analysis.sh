3770939926b16b619bf4cf8c6773b3a209701157
Sergio Ocampo <ocamp020@github.com>
2019-05-22 11:54:57 -0500

    Tax Timinig Merged with Transition Branch

--- analysis.sh
+++ analysis.sh
@@ -2,5 +2,5 @@
 module load gcc/4.8.5
 cd RA_Guvenen/GKKC_Code/
 make clean
-rm -f log_Transition.txt
-make GKK_Main > log_Transition.txt
+rm -f log_timing_tax_reform.txt
+make GKK_Main > log_timing_tax_reform.txt
