ca3100760fe9e2e386ed89792a8744352d4084b8
Sergio Ocampo <ocamp020@github.com>
2020-01-23 15:12:29 -0500

    Transition: Add debt to SS and update R every 3 periods (plus new criteria and print results in Find_DBN_Transition)

--- programfunctions.f90
+++ programfunctions.f90
@@ -5547,7 +5547,6 @@ SUBROUTINE FIND_DBN_Transition()
 			deallocate( YGRID_t, MBGRID_t, Cons_t, Hours_t, Aprime_t )
 		! Solve for New Steady State
 			CALL FIND_DBN_EQ
-				CALL Asset_Grid_Threshold(Y_a_threshold,agrid_t,na_t)
 				GBAR_exp  = GBAR
 				QBAR_exp  = QBAR 
 				NBAR_exp  = NBAR  
