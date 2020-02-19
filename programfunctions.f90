40fe150d239e6fddef1793398881e1be95788a06
Sergio Ocampo <ocamp020@github.com>
2020-02-19 13:27:02 -0600

    Solving benchmark and tax reform

--- programfunctions.f90
+++ programfunctions.f90
@@ -4049,7 +4049,7 @@ SUBROUTINE FIND_DBN_EQ()
 
 		! Everyone in MaxAge dies. Those who die, switch to z2, lambda2 and start at ne/2+1 and x=1
 	    age1=MaxAge
-	    !$omp parallel do reduction(+:DBN2) private(x1,a1,lambda1,e1,z2,lambda2)
+	    ! $omp parallel do reduction(+:DBN2) private(x1,a1,lambda1,e1,z2,lambda2)
 	    DO z1=1,nz
 	    DO x1=1,nx
 	    DO a1=1,na
@@ -4073,7 +4073,7 @@ SUBROUTINE FIND_DBN_EQ()
 	    !$omp barrier  
 
 		! retirees "e" stays the same for benefit retirement calculation purposes
-		!$omp parallel do reduction(+:DBN2) private(x1,age1,a1,lambda1,e1,z2,lambda2,x2)
+		! $omp parallel do reduction(+:DBN2) private(x1,age1,a1,lambda1,e1,z2,lambda2,x2)
 		DO z1=1,nz
 	    DO x1=1,nx
 	    DO age1=RetAge-1, MaxAge-1
@@ -4111,7 +4111,7 @@ SUBROUTINE FIND_DBN_EQ()
 	    !$omp barrier
 	    
 	    ! Working age agents
-	    !$omp parallel do reduction(+:DBN2) private(x1,age1,a1,lambda1,e1,z2,lambda2,x2,e2)
+	    ! $omp parallel do reduction(+:DBN2) private(x1,age1,a1,lambda1,e1,z2,lambda2,x2,e2)
 	    DO z1=1,nz
 	    DO x1=1,nx
 	    DO age1=1,RetAge-2
