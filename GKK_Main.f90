f329d7835b97fb41b380f14ddbbbfde4a7fd2e63
Sergio Ocampo <ocamp020@github.com>
2020-03-26 12:14:50 -0400

    Updated Simulation_Module

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -64,7 +64,7 @@ PROGRAM main
 		! If compute_bench==.true. then just read resutls
 		! If compute_bench==.false. then solve for benchmark and store results
 		Tax_Reform    = .true.
-			compute_bench = .true.
+			compute_bench = .false.
 			compute_exp   = .false.
 			compute_exp_pf= .false.
 				Fixed_PF        = .false.
