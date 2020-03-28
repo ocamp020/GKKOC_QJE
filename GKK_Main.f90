af7001c73710811add96cd56e214a0fe971157cf
Sergio Ocampo <ocamp020@github.com>
2020-03-28 15:32:02 -0400

    Bequest Calibration - chi=25.00

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -126,7 +126,7 @@ PROGRAM main
 		beta   	= 0.9240_dp! 0.9475_dp ! params(1) !
 		mu_z   	= params(2) ! this is just shifting the z grids. it is zero now.
 		rho_z  	= 0.1_dp ! params(3) 
-		sigma_z_eps      =  0.0770_dp! 0.072_dp !0.115_dp ! params(4) ! 0.01_dp ! ! 
+		sigma_z_eps      =  0.0765_dp! 0.072_dp !0.115_dp ! params(4) ! 0.01_dp ! ! 
 		sigma_lambda_eps = 0.305_dp ! params(5)
 		gamma  	=  0.46_dp !  0.471_dp ! params(6) ! 
 		Params =[beta, mu_z, rho_z, sigma_z_eps, sigma_lambda_eps, gamma] 
