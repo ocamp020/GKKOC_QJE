e0c76ec130bff8413f632fbecaa87119fee0a14f
Sergio Ocampo <ocamp020@github.com>
2020-03-29 20:08:34 -0400

    Corporate Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -123,19 +123,19 @@ PROGRAM main
 		! Calibration to book value and rho_z=0.1 and x_hi=10
 		Params =[ 0.9473_dp, 0.00_dp, 0.1_dp, 0.0352_dp , 0.307_dp, 0.46_dp ] 
 		
-		beta   	= 0.9349_dp! 0.9475_dp ! params(1) !
+		beta   	= 0.9348_dp! 0.9475_dp ! params(1) !
 		mu_z   	= params(2) ! this is just shifting the z grids. it is zero now.
 		rho_z  	= 0.1_dp ! params(3) 
-		sigma_z_eps      =  0.0843_dp! 0.072_dp ! params(4) !
-		sigma_lambda_eps = 0.295_dp ! 0.305_dp ! params(5)
-		gamma  	=  0.469_dp ! 0.46_dp !  params(6) ! 
+		sigma_z_eps      =  0.0844_dp! 0.072_dp ! params(4) !
+		sigma_lambda_eps = 0.297_dp ! 0.305_dp ! params(5)
+		gamma  	=  0.465_dp ! 0.46_dp !  params(6) ! 
 		Params =[beta, mu_z, rho_z, sigma_z_eps, sigma_lambda_eps, gamma] 
 		
 		sigma  	= 4.0_dp
 		phi    	= (1.0_dp-gamma)/gamma
 
 		! Bequeset parameters chi_bq*(bq+bq_0)^(1-sigma)
-			chi_bq = 32.00_dp ! Scaling 
+			chi_bq = 3300_dp ! Scaling 
 			bq_0   = 01.10_dp ! Level shift 
 
 		! Corporate Sector
