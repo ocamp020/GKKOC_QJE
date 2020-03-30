a0daee57a0575af395415996a2753ea88dfc0bb8
Sergio Ocampo <ocamp020@github.com>
2020-03-30 10:11:49 -0400

    Corporate Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -126,7 +126,7 @@ PROGRAM main
 		beta   	= 0.9337_dp! 0.9475_dp ! params(1) !
 		mu_z   	= params(2) ! this is just shifting the z grids. it is zero now.
 		rho_z  	= 0.1_dp ! params(3) 
-		sigma_z_eps      =  0.0853_dp! 0.072_dp ! params(4) !
+		sigma_z_eps      =  0.0851_dp! 0.072_dp ! params(4) !
 		sigma_lambda_eps = 0.303_dp ! 0.305_dp ! params(5)
 		gamma  	=  0.4590_dp ! 0.46_dp !  params(6) ! 
 		Params =[beta, mu_z, rho_z, sigma_z_eps, sigma_lambda_eps, gamma] 
@@ -139,7 +139,7 @@ PROGRAM main
 			bq_0   = 01.20_dp ! Level shift 
 
 		! Corporate Sector
-			A_C    = 0.9235_dp
+			A_C    = 0.9230_dp
 
 		x_hi	= 5.00_dp
 		x_lo	= 1.00_dp
