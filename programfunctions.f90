8f70df5f9afe3c1a59bb6398eaf5de54240e85d0
Sergio Ocampo <ocamp020@github.com>
2020-03-30 13:16:22 -0400

    Corporate Tax Reform

--- programfunctions.f90
+++ programfunctions.f90
@@ -4477,7 +4477,8 @@ SUBROUTINE FIND_DBN_EQ()
 		& ' 	DBN_diff=', DBN_dist,'A=',sum( sum(sum(sum(sum(sum(DBN1,6),5),4),3),1)*agrid ),&
 		& 'W=',wage,'R=',R,'P=',P,'Q=',QBAR, &
 		& 'K_C/A=',100.0_dp*K_C/Wealth,'L_C/N=',100.0_dp*L_C/NBAR,'Y_C/Y=',100.0_dp*YBAR_C/YBAR,'K_C=',K_C,'L_C=',L_C,'Iter=',simutime
-	12346 format (A,E12.5,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,I5)
+	12346 format &
+	& (A,E12.5,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,F7.3,X,X,A,I5)
 	print*,' '; print*,'-----------------------------------------------------------------------------'
 	print*,' '
 
