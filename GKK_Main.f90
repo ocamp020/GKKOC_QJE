bc87f9988bb94aa3f7ecf2796dff5fd7ed575b4d
Sergio Ocampo <ocamp020@github.com>
2020-03-31 20:16:39 -0400

    Corporate Tax Reform

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -123,11 +123,11 @@ PROGRAM main
 		! Calibration to book value and rho_z=0.1 and x_hi=10
 		Params =[ 0.9473_dp, 0.00_dp, 0.1_dp, 0.0352_dp , 0.307_dp, 0.46_dp ] 
 		
-		beta   	= 0.9385_dp! 0.9475_dp ! params(1) !
+		beta   	= 0.9389_dp! 0.9475_dp ! params(1) !
 		mu_z   	= params(2) ! this is just shifting the z grids. it is zero now.
 		rho_z  	= 0.1_dp ! params(3) 
-		sigma_z_eps      =  0.0870_dp! 0.072_dp ! params(4) !
-		sigma_lambda_eps = 0.303_dp ! 0.305_dp ! params(5)
+		sigma_z_eps      =  0.0860_dp! 0.072_dp ! params(4) !
+		sigma_lambda_eps = 0.305_dp ! 0.305_dp ! params(5)
 		gamma  	=  0.4590_dp ! 0.46_dp !  params(6) ! 
 		Params =[beta, mu_z, rho_z, sigma_z_eps, sigma_lambda_eps, gamma] 
 		
