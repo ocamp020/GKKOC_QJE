d3a2c4edc1d4a851f7e2dfbc149c130a09e1d174
Sergio Ocampo <ocamp020@github.com>
2020-05-02 18:21:01 -0400

    Model 2.0 luxury, Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -166,7 +166,7 @@ PROGRAM main
 
 		! Debt/Output = 1.3, high luxury component
 			! Main Parameters 
-				beta   	= 0.9589_dp ! 0.9404_dp (Value without estate tax)! 0.9475_dp (value in old benchmark) ! params(1) !
+				beta   	= 0.9590_dp ! 0.9404_dp (Value without estate tax)! 0.9475_dp (value in old benchmark) ! params(1) !
 				sigma_z_eps      = 0.0783_dp ! 0.0867_dp (Value without estate tax) ! 0.072_dp (value in old benchmark) ! params(4) !
 				sigma_lambda_eps = 0.307_dp ! 0.309_dp (Value without estate tax) ! 0.305_dp (value in old benchmark) ! params(5)
 				gamma  	=  0.4500_dp ! 0.4580_dp (Value without estate tax) ! 0.46_dp (value in old benchmark) !  params(6) ! 
@@ -174,7 +174,7 @@ PROGRAM main
 
 			! Bequeset parameters chi_bq*(bq+bq_0)^(1-sigma)
 				bq_0   = 02.00_dp ! Level shift 00.30_dp (value without estate tax)
-				chi_u  = 00.79_dp ! Scaling 03.55_dp (value without estate tax)
+				chi_u  = 00.82_dp ! Scaling 03.55_dp (value without estate tax)
 				chi_bq = chi_u*(1.0_dp-tau_bq) ! Auxiliary parameter for FOC and EGM
 
 			! Capital Market
