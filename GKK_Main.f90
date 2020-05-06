c682f96bbe0964c2c9ada6d03637e2c3837b36b9
Sergio Ocampo <ocamp020@github.com>
2020-05-05 20:01:34 -0400

    Model 2.0 lambda=1, Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -207,7 +207,7 @@ PROGRAM main
 		! Debt/Output = 1.3, lambda = 1
 			! Main Parameters 
 				beta   	= 0.9588_dp ! 0.9404_dp (Value without estate tax)! 0.9475_dp (value in old benchmark) ! params(1) !
-				sigma_z_eps      = 0.1600_dp ! 0.0867_dp (Value without estate tax) ! 0.072_dp (value in old benchmark) ! params(4) !
+				sigma_z_eps      = 0.1800_dp ! 0.0867_dp (Value without estate tax) ! 0.072_dp (value in old benchmark) ! params(4) !
 				sigma_lambda_eps = 0.307_dp ! 0.309_dp (Value without estate tax) ! 0.305_dp (value in old benchmark) ! params(5)
 				gamma  	=  0.4500_dp ! 0.4580_dp (Value without estate tax) ! 0.46_dp (value in old benchmark) !  params(6) ! 
 				sigma  	= 4.0_dp
