52b529a5c0b0dfd3dd3083fa8343e047fece178c
Sergio Ocampo <ocamp020@github.com>
2020-05-21 09:08:09 -0400

    sigma 3 - Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -308,7 +308,7 @@ PROGRAM main
 		! ! Debt/Output = 1.3 - Sigma =3
 			! Main Parameters 
 				beta   	= 0.9586_dp ! 0.9404_dp (Value without estate tax)! 0.9475_dp (value in old benchmark) ! params(1) !
-				sigma_z_eps      = 0.0783_dp ! 0.0867_dp (Value without estate tax) ! 0.072_dp (value in old benchmark) ! params(4) !
+				sigma_z_eps      = 0.0800_dp ! 0.0867_dp (Value without estate tax) ! 0.072_dp (value in old benchmark) ! params(4) !
 				sigma_lambda_eps = 0.307_dp ! 0.309_dp (Value without estate tax) ! 0.305_dp (value in old benchmark) ! params(5)
 				gamma  	=  0.4500_dp ! 0.4580_dp (Value without estate tax) ! 0.46_dp (value in old benchmark) !  params(6) ! 
 				sigma  	= 3.0_dp
