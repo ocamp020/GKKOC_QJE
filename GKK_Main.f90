aebc188ec3a7e9ff96a9bebee74d6e372b214cc2
Sergio Ocampo <ocamp020@github.com>
2020-05-22 19:50:41 -0400

    lambda 1.5 + no fee: Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -59,9 +59,9 @@ PROGRAM main
 		Calibration_Switch = .false.
 		! If compute_bench==.true. then just read resutls
 		! If compute_bench==.false. then solve for benchmark and store results
-		Tax_Reform    = .false.
-			compute_bench = .false.
-			compute_exp   = .false.
+		Tax_Reform    = .true.
+			compute_bench = .true.
+			compute_exp   = .true.
 			compute_exp_pf= .false.
 				Fixed_PF        = .true.
 				Fixed_PF_interp = .true.
@@ -77,7 +77,7 @@ PROGRAM main
 		Tax_Reform_Decomposition = .false.
 		compute_exp_fixed_prices_and_taxes = .false.
 
-		Opt_Tax       = .true.
+		Opt_Tax       = .false.
 			Opt_Tax_KW    = .false. ! true=tau_K, false=tau_W
 		Opt_Tax_K_and_W = .false.
 		Tax_Reform_KW   = .false.
@@ -91,7 +91,7 @@ PROGRAM main
 			balance_tau_L  = .true. ! true=tau_L, false=tau_K or tau_W depending on Opt_Tax_KW
 			Opt_Tax_KW_TR  = .false. ! true=tau_K, false=tau_W
 		
-		Simul_Switch  = .true.
+		Simul_Switch  = .false.
 
 
 
@@ -262,7 +262,26 @@ PROGRAM main
 		! 		theta(zi)    = 1.00_dp+(2.82_dp-1.00_dp)/(nz-1)*(real(zi,8)-1.0_dp)
 		! 		enddo
 
-		! Debt/Output = 1.5, lambda = 2
+		! ! Debt/Output = 1.5, lambda = 1.5
+		! 	! Main Parameters 
+		! 		beta   	= 0.9635_dp ! 0.9404_dp (Value without estate tax)! 0.9475_dp (value in old benchmark) ! params(1) !
+		! 		sigma_z_eps      = 0.289_dp ! 0.0867_dp (Value without estate tax) ! 0.072_dp (value in old benchmark) ! params(4) !
+		! 		sigma_lambda_eps = 0.309_dp ! 0.309_dp (Value without estate tax) ! 0.305_dp (value in old benchmark) ! params(5)
+		! 		gamma  	= 0.4450_dp ! 0.4580_dp (Value without estate tax) ! 0.46_dp (value in old benchmark) !  params(6) ! 
+		! 		sigma  	= 4.0_dp
+		! 		x_hi	= 1.50_dp
+
+		! 	! Bequeset parameters chi_bq*(bq+bq_0)^(1-sigma)
+		! 		bq_0   = 00.30_dp ! Level shift 00.30_dp (value without estate tax)
+		! 		chi_u  = 00.05_dp ! Scaling 03.55_dp (value without estate tax)
+		! 		chi_bq = chi_u*(1.0_dp-tau_bq) ! Auxiliary parameter for FOC and EGM
+
+		! 	! Capital Market
+		! 		do zi=1,nz
+		! 		theta(zi)    = 1.00_dp+(2.82_dp-1.00_dp)/(nz-1)*(real(zi,8)-1.0_dp)
+		! 		enddo
+
+		! Debt/Output = 1.5, lambda = 1.5, no bequest fee
 			! Main Parameters 
 				beta   	= 0.9635_dp ! 0.9404_dp (Value without estate tax)! 0.9475_dp (value in old benchmark) ! params(1) !
 				sigma_z_eps      = 0.289_dp ! 0.0867_dp (Value without estate tax) ! 0.072_dp (value in old benchmark) ! params(4) !
@@ -280,6 +299,10 @@ PROGRAM main
 				do zi=1,nz
 				theta(zi)    = 1.00_dp+(2.82_dp-1.00_dp)/(nz-1)*(real(zi,8)-1.0_dp)
 				enddo
+
+			! No bequest fees
+				bq_fee = 0.00_dp
+
 
 		! ! Debt/Output = 1.3 - No bequest fees 
 		! 	! Main Parameters 
@@ -431,6 +454,7 @@ PROGRAM main
 			! Result_Folder = './Revision/Model_2.0_lambda_3/' 
 			! Result_Folder = './Revision/Model_2.0_lambda_2/' 
 			Result_Folder = './Revision/Model_2.0_lambda_1.5/' 
+			Result_Folder = './Revision/Model_2.0_lambda_1.5_no_fee/' 
 			! Result_Folder = './Revision/Model_2.0_no_fee/'
 			! Result_Folder = './Revision/Model_2.0_p1_high/'
 		else if ((Progressive_Tax_Switch.eqv..true.).and.(NSU_Switch.eqv..true.)) then 
@@ -818,7 +842,7 @@ Subroutine Solve_Benchmark(compute_bench,Simul_Switch)
 		! print*,"	Efficiency Computation"
 		! CALL Hsieh_Klenow_Efficiency(solving_bench)
 
-		! STOP
+		STOP
 		
 
 end Subroutine Solve_Benchmark
