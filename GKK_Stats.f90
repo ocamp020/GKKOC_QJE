5119f8f8b9ec67f6f0a46be0c2a717e2b798587f
Sergio Ocampo <ocamp020@github.com>
2020-07-13 20:07:11 -0400

    No Constraint - Check CE1

--- GKK_Stats.f90
+++ GKK_Stats.f90
@@ -1804,15 +1804,32 @@ SUBROUTINE COMPUTE_WELFARE_GAIN()
 		    !    & 100.0_dp*sum(Cons_Eq_Welfare(age,:,:,:,:,:)*DBN_bench(age,:,:,:,:,:))/sum(DBN_bench(age,:,:,:,:,:))
 		ENDDO
 
-		print*,'Test X for age=81 and z=8 and x=1'
-		do age=1,na 
-		print*,age, ((ValueFunction_exp(81,age,8,3,3,1)-Bq_Value_bench(81,age,8,3,3,1))/&
-		    									& (ValueFunction_Bench(81,age,8,3,3,1)-Bq_Value_bench(81,age,8,3,3,1)) ) &
-                                				&  ** ( 1.0_DP / ( gamma* (1.0_DP-sigma)) )-1.0_DP, &
-				& ValueFunction_exp(81,age,8,3,3,1)  -Bq_Value_bench(81,age,8,3,3,1)  , & 
-				& ValueFunction_Bench(81,age,8,3,3,1)-Bq_Value_bench(81,age,8,3,3,1),&
-				& ValueFunction_exp(81,age,8,3,3,1), Bq_Value_bench(81,age,8,3,3,1), DBN_bench(81,age,8,3,3,1)
-		enddo 
+		! Check for NaN
+		DO age=1,MaxAge
+		DO zi=1,nz
+		DO xi=1,nx
+		DO ai=1,na
+		DO lambdai=1,nlambda
+		DO ei=1, ne
+			if isnan(Cons_Eq_Welfare(age,ai,zi,lambdai,ei,xi)).and.(DBN_bench(age,ai,zi,lambdai,ei,xi)<1e-6) then
+				Cons_Eq_Welfare(age,ai,zi,lambdai,ei,xi) = 0.0_dp
+			endif
+		ENDDO
+		ENDDO
+		ENDDO
+		ENDDO
+		ENDDO
+		ENDDO
+
+		! print*,'Test X for age=81 and z=8 and x=1'
+		! do age=1,na 
+		! print*,age, ((ValueFunction_exp(81,age,8,3,3,1)-Bq_Value_bench(81,age,8,3,3,1))/&
+		!     									& (ValueFunction_Bench(81,age,8,3,3,1)-Bq_Value_bench(81,age,8,3,3,1)) ) &
+  !                               				&  ** ( 1.0_DP / ( gamma* (1.0_DP-sigma)) )-1.0_DP, &
+		! 		& ValueFunction_exp(81,age,8,3,3,1)  -Bq_Value_bench(81,age,8,3,3,1)  , & 
+		! 		& ValueFunction_Bench(81,age,8,3,3,1)-Bq_Value_bench(81,age,8,3,3,1),&
+		! 		& ValueFunction_exp(81,age,8,3,3,1), Bq_Value_bench(81,age,8,3,3,1), DBN_bench(81,age,8,3,3,1)
+		! enddo 
 
 		CE_NEWBORN = 100.0_DP*sum(Cons_Eq_Welfare(1,:,:,:,:,:)*DBN_bench(1,:,:,:,:,:))/sum(DBN_bench(1,:,:,:,:,:))
 
