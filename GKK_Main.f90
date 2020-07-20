55b41f9222ad24e8661bd0eab6bb833037fbd91e
Sergio Ocampo <ocamp020@github.com>
2020-07-20 13:35:13 -0400

    Corp. Sector - Two cpaital Markets Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -130,7 +130,7 @@ PROGRAM main
 
 		! Corporate Sector
 			! A_C    = 0.0_dp ! 
-			A_C    = 0.9700_dp ! for Corp model ! 0.9409_dp (value without estate tax)
+			A_C    = 1.1000_dp ! for Corp model ! 0.9409_dp (value without estate tax)
 
 		if (A_C.eq.0.0_dp) then
 		
@@ -164,8 +164,8 @@ PROGRAM main
 		! Corporate Sector
 
 		! Main Parameters 
-			beta   	= 0.9700_dp 
-			sigma_z_eps      = 0.320_dp 
+			beta   	= 0.9800_dp 
+			sigma_z_eps      = 0.330_dp 
 			sigma_lambda_eps = 0.309_dp
 			gamma  	=  0.4450_dp
 			sigma  	= 4.0_dp
