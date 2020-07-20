eedaea8e8be60203704b71683a9c05bcd76424a8
Sergio Ocampo <ocamp020@github.com>
2020-07-20 16:28:17 -0400

    Corp. Sector - Two cpaital Markets Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -164,8 +164,8 @@ PROGRAM main
 		! Corporate Sector
 
 		! Main Parameters 
-			beta   	= 0.9620_dp 
-			sigma_z_eps      = 0.355_dp 
+			beta   	= 0.9610_dp 
+			sigma_z_eps      = 0.357_dp 
 			sigma_lambda_eps = 0.309_dp
 			gamma  	=  0.4450_dp
 			sigma  	= 4.0_dp
