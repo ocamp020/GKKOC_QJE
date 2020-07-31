71cdef484edc505b5c09d1886703af3efc46fef4
Sergio Ocampo <ocamp020@github.com>
2020-07-31 09:04:35 -0400

    Interest Rate Wedge (RW) - Tax Reform

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -2384,8 +2384,8 @@ Subroutine Solve_Opt_Tax(Opt_Tax_KW,Simul_Switch)
 		print*,''
     	OPEN (UNIT=77, FILE=trim(Result_Folder)//'Stats_by_tau_w.txt', STATUS='replace')
     	
-    	tau_grid_min  = 10
-    	tau_grid_max  = 50
+    	tau_grid_min  = 00
+    	tau_grid_max  = 15
     	tau_grid_step = 1
 
     	! Set Y_a_threshold
