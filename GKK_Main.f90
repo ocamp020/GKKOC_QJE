367aaa62534fbeb2b93cf8b45a643babc3ae1721
Sergio Ocampo <ocamp020@github.com>
2020-08-10 11:50:41 -0400

    X Transition with Low NB - Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -648,8 +648,8 @@ Subroutine Solve_Experiment(compute_exp,Simul_Switch)
 		! Wealth tax: minimum wealth tax to consider and increments for balancing budget
 		tauWmin_bt=0.00_DP
 		tauWinc_bt=0.000_DP ! Minimum tax below threshold and increments
-		tauWmin_at=0.010_DP
-		tauWinc_at=0.001_DP ! Minimum tax above threshold and increments
+		tauWmin_at=0.011_DP
+		tauWinc_at=0.0006_DP ! Minimum tax above threshold and increments
 		if (KeepSSatBench .eq. 0) then
 		tauWmin_at = tauW_at
 		tauWinc_at = 0.0005_dp
