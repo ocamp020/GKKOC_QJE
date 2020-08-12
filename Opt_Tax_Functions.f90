e62d61515b43b5c37e89b81f4eb34147da5c5fe8
Sergio Ocampo <ocamp020@github.com>
2020-08-12 17:03:59 -0400

    Model 2.1 CKK - OTK

--- Opt_Tax_Functions.f90
+++ Opt_Tax_Functions.f90
@@ -37,7 +37,7 @@ FUNCTION EQ_WELFARE_GIVEN_TauK(tauk_in)
 	tauW_at = 0.0_DP
 
 	GBAR_exp = 0.0_DP
-	DO WHILE (  abs(100.0_DP*(1.0_DP-GBAR_exp/GBAR_bench)) .gt. 0.01 ) ! as long as the difference is greater than 0.05% continue
+	DO WHILE (  abs(100.0_DP*(1.0_DP-GBAR_exp/GBAR_bench)) .gt. 0.001 ) ! as long as the difference is greater than 0.05% continue
 	    CALL FIND_DBN_EQ
 	    CALL GOVNT_BUDGET_OPT
 	    GBAR_exp = GBAR    
@@ -89,7 +89,7 @@ FUNCTION EQ_WELFARE_GIVEN_TauW(tauW_in)
 	tauW_at =  tauW_in
 
 	GBAR_exp = 0.0_DP
-	DO WHILE (  abs(100.0_DP*(1.0_DP-GBAR_exp/GBAR_bench)) .gt. 0.01 ) ! as long as the difference is greater than 0.05% continue
+	DO WHILE (  abs(100.0_DP*(1.0_DP-GBAR_exp/GBAR_bench)) .gt. 0.001 ) ! as long as the difference is greater than 0.05% continue
 	    CALL FIND_DBN_EQ
 	    CALL GOVNT_BUDGET_OPT
 	    GBAR_exp = GBAR    
@@ -393,7 +393,7 @@ SUBROUTINE GOVNT_BUDGET_OPT()
 
 	! OBTAIN NEW PSI IF GOVETNMENT BUDGET DOES NOT BALANCE
 	if (solving_bench .eq. 0) then
-	    IF (  abs(100.0_DP*(1.0_DP-GBAR/GBAR_bench)) .gt. 0.01 ) THEN
+	    IF (  abs(100.0_DP*(1.0_DP-GBAR/GBAR_bench)) .gt. 0.001 ) THEN
 	        !new_psi =  ( BT_EARNINGS - GBAR_bench -  SSC_Payments   + GBAR_NL ) / A_EARNINGS
 	        new_psi = 1.0_dp - (  GBAR_bench + SSC_Payments - GBAR_NL ) / BT_EARNINGS
 	        PRINT*,'NEW PSI=',new_psi,'Old Psi=',psi
