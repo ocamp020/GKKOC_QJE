2288762ffbb8b6f6e6c172e274969a30b28393d7
Sergio Ocampo <ocamp020@github.com>
2020-08-13 21:21:23 -0400

    Model 2.1 IPO - Debugging

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -159,7 +159,7 @@ PROGRAM main
 			! IPO variables 
 				p1_x = 0.05_dp ! Prob normal firm shuts down
 				p2_x = 0.01_dp ! Prob public firm shuts down
-				p3_x = 0.001_dp ! Prob going public 
+				p3_x = 0.0001_dp ! Prob going public 
 
 		else
 
