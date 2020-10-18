b2d50dd562c54be641164936f6eb238ba1627b51
Sergio Ocampo <ocamp020@github.com>
2020-10-18 19:40:44 -0400

    Calibration - Model_2.1 with Debt/GDP=2

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -150,7 +150,7 @@ PROGRAM main
 
 			! Capital Market
 				do zi=1,nz
-				theta(zi)    = 1.00_dp+(4.53s_dp-1.00_dp)/(nz-1)*(real(zi,8)-1.0_dp)
+				theta(zi)    = 1.00_dp+(4.53_dp-1.00_dp)/(nz-1)*(real(zi,8)-1.0_dp)
 				enddo
 
 			! No bequest fees
