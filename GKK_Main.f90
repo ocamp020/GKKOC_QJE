6548e68545183e1c1de8d6faee0e84211710af7f
Sergio Ocampo <ocamp020@github.com>
2020-10-26 12:19:40 -0400

    Model_2.1 - CKK with lump sum bequests - Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -145,8 +145,9 @@ PROGRAM main
 
 			! Bequeset parameters chi_bq*(bq+bq_0)^(1-sigma)
 				bq_0   = 00.30_dp ! Level shift 00.30_dp (value without estate tax)
-				chi_u  = 09.90_dp ! Scaling 03.55_dp (value without estate tax)
+				chi_u  = 00.00_dp ! Scaling 03.55_dp (value without estate tax)
 				chi_bq = chi_u*(1.0_dp-tau_bq) ! Auxiliary parameter for FOC and EGM
+				tau_bq = 0.0_dp 
 
 			! Capital Market
 				do zi=1,nz
