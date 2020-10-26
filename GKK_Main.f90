6bdbc1f617ed8581a0ada356e22d13dd3d5245cd
Sergio Ocampo <ocamp020@github.com>
2020-10-26 12:00:12 -0400

    Model_2.1 - CKK with lump sum bequests - Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -59,9 +59,9 @@ PROGRAM main
 		Calibration_Switch = .false.
 		! If compute_bench==.true. then just read resutls
 		! If compute_bench==.false. then solve for benchmark and store results
-		Tax_Reform    = .false.
-			compute_bench = .false.
-			compute_exp   = .false.
+		Tax_Reform    = .true.
+			compute_bench = .true.
+			compute_exp   = .true.
 			compute_exp_pf= .false.
 				Fixed_PF        = .true.
 				Fixed_PF_interp = .true.
@@ -81,7 +81,7 @@ PROGRAM main
 
 		compute_exp_fixed_prices_and_taxes = .false.
 
-		Opt_Tax       = .true.
+		Opt_Tax       = .false.
 			Opt_Tax_KW    = .false. ! true=tau_K, false=tau_W
 
 		Opt_Threshold = .false.
@@ -220,23 +220,23 @@ PROGRAM main
 	! Resutls Folder
 	if (A_C.eq.0.0_dp) then 
  		if ((Progressive_Tax_Switch.eqv..false.).and.(NSU_Switch.eqv..true.)) then 
-			Result_Folder = './Revision/Model_2.1_CKK/'
+			Result_Folder = './Revision/Model_2.1_CKK_bequest/'
 		else if ((Progressive_Tax_Switch.eqv..true.).and.(NSU_Switch.eqv..true.)) then 
-			Result_Folder = './Revision/Model_2.1_CKK_PT/' 
+			Result_Folder = './Revision/Model_2.1_CKK_bequest_PT/' 
 		else if ((Progressive_Tax_Switch.eqv..false.).and.(NSU_Switch.eqv..false.)) then 
-			Result_Folder = './Revision/Model_2.1_CKK_SU/' 
+			Result_Folder = './Revision/Model_2.1_CKK_bequest_SU/' 
 		else if ((Progressive_Tax_Switch.eqv..true.).and.(NSU_Switch.eqv..false.)) then 
-			Result_Folder = './Revision/Model_2.1_CKK_PT_SU/' 
+			Result_Folder = './Revision/Model_2.1_CKK_bequest_PT_SU/' 
 		end if
 	else 
  		if ((Progressive_Tax_Switch.eqv..false.).and.(NSU_Switch.eqv..true.)) then 
-			Result_Folder = './Revision/Model_2.1_CKK_Corp/' 
+			Result_Folder = './Revision/Model_2.1_CKK_bequest_Corp/' 
 		else if ((Progressive_Tax_Switch.eqv..true.).and.(NSU_Switch.eqv..true.)) then 
-			Result_Folder = './Revision/Model_2.1_CKK_Corp_PT/' 
+			Result_Folder = './Revision/Model_2.1_CKK_bequest_Corp_PT/' 
 		else if ((Progressive_Tax_Switch.eqv..false.).and.(NSU_Switch.eqv..false.)) then 
-			Result_Folder = './Revision/Model_2.1_CKK_Corp_SU/' 
+			Result_Folder = './Revision/Model_2.1_CKK_bequest_Corp_SU/' 
 		else if ((Progressive_Tax_Switch.eqv..true.).and.(NSU_Switch.eqv..false.)) then 
-			Result_Folder = './Revision/Model_2.1_CKK_Corp_PT_SU/' 
+			Result_Folder = './Revision/Model_2.1_CKK_bequest_Corp_PT_SU/' 
 		end if
 	endif 
 
