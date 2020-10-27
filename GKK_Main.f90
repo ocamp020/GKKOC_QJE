7d3360cbae5be61a4c420b9a429a897e347f45f9
Sergio Ocampo <ocamp020@github.com>
2020-10-27 13:57:45 -0400

    No X shock - Calibration

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -150,7 +150,7 @@ PROGRAM main
 
 			! Capital Market
 				do zi=1,nz
-				theta(zi)    = 1.00_dp+(2.58_dp-1.00_dp)/(nz-1)*(real(zi,8)-1.0_dp)
+				theta(zi)    = 1.00_dp+(2.60_dp-1.00_dp)/(nz-1)*(real(zi,8)-1.0_dp)
 				enddo
 
 			! No bequest fees
