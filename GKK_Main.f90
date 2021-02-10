32b0f0040654baa596383ec5838db3b6ef140f2b
Sergio Ocampo <ocamp020@github.com>
2021-02-10 08:17:10 -0500

    Optimal Tau W

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -59,7 +59,7 @@ PROGRAM main
 		Calibration_Switch = .false.
 		! If compute_bench==.true. then just read resutls
 		! If compute_bench==.false. then solve for benchmark and store results
-		Tax_Reform    = .true.
+		Tax_Reform    = .false.
 			compute_bench = .false.
 			compute_exp   = .false.
 			compute_exp_pf= .false.
@@ -81,8 +81,8 @@ PROGRAM main
 
 		compute_exp_fixed_prices_and_taxes = .false.
 
-		Opt_Tax       = .false.
-			Opt_Tax_KW    = .true. ! true=tau_K, false=tau_W
+		Opt_Tax       = .true.
+			Opt_Tax_KW    = .false. ! true=tau_K, false=tau_W
 
 		Opt_Threshold = .false.
 
@@ -370,10 +370,10 @@ PROGRAM main
 
 			folder_aux = Result_Folder
 			if (Opt_Tax_KW) then 
-				Result_Folder = trim(folder_aux)//'Opt_Tax_K_wide_grid/'
+				Result_Folder = trim(folder_aux)//'Opt_Tax_K/'
 			else 
 				if (KeepSSatBench .eq. 1) then 
-				Result_Folder = trim(folder_aux)//'Opt_Tax_W_wide_grid/'
+				Result_Folder = trim(folder_aux)//'Opt_Tax_W/'
 				else 
 				Result_Folder = trim(folder_aux)//'Opt_Tax_W_SS/'
 				endif 
@@ -2343,7 +2343,7 @@ Subroutine Solve_Opt_Tax(Opt_Tax_KW,Simul_Switch)
 
 	! Set flag for reading results or computing optimal taxes
 		read_results = .false.
-		load_seed    = .true.
+		load_seed    = .false.
 
 
 	if (read_results.eqv..false.) then 
@@ -2380,8 +2380,8 @@ Subroutine Solve_Opt_Tax(Opt_Tax_KW,Simul_Switch)
 		print*,''
     	OPEN (UNIT=77, FILE=trim(Result_Folder)//'Stats_by_tau_w.txt', STATUS='replace')
     	
-    	tau_grid_min  = -50
-    	tau_grid_max  = 50
+    	tau_grid_min  = 22
+    	tau_grid_max  = 38
     	tau_grid_step = 1
 
     	! Set Y_a_threshold
@@ -2390,7 +2390,7 @@ Subroutine Solve_Opt_Tax(Opt_Tax_KW,Simul_Switch)
 			Wealth_factor = Y_a_threshold/W_bench
 
 		! Set high psi
-		! psi = 0.87_dp
+		psi = 0.82_dp
 	endif 
     	WRITE(UNIT=77, FMT=*) 'tauK ', 'tauW_at ', 'psi ', 'GBAR_K/Tax_Rev_bench ', &
 		      & 'KBAR ','QBAR ','TFP ','NBAR ','YBAR ','Y_Growth ', 'CBAR ','C_Growth ', 'wage ','R ', &
