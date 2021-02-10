24d961772d6aa516f3ec62c851593a0c0eb6c473
Sergio Ocampo <ocamp020@github.com>
2021-02-10 13:49:38 -0500

    Tax Reform

--- GKK_Main.f90
+++ GKK_Main.f90
@@ -653,7 +653,7 @@ Subroutine Solve_Experiment(compute_exp,Simul_Switch)
 		tauWmin_bt=0.00_DP
 		tauWinc_bt=0.000_DP ! Minimum tax below threshold and increments
 		tauWmin_at=0.002_DP
-		tauWinc_at=0.001_DP ! Minimum tax above threshold and increments
+		tauWinc_at=0.0005_DP ! Minimum tax above threshold and increments
 		if (KeepSSatBench .eq. 0) then
 		tauWmin_at = tauW_at
 		tauWinc_at = 0.0005_dp
