b32a435abc9d0a36106c23442c5eb0da90e55725
Sergio Ocampo <ocamp020@github.com>
2021-02-19 16:45:41 -0500

    Check Lifetime Returns

--- Simulation_Module.f90
+++ Simulation_Module.f90
@@ -2587,7 +2587,7 @@ SUBROUTINE  Simulation_Life_Cycle_Asset_Return_Panel(bench_indx)
 	DO i = 1, sample_size
 	DO age = 1,
 	WRITE  (UNIT=10, FMT='(I7,A,I2,A,I1,A,F12.4,A,I1,A,F12.4)') &
-		& i,',',age+19,',',Panel_Death(i,age),',',panel_r(i,age),',',panel_z(i,age),',',panel_a(i,age)
+		& i,',',age+19,',',Panel_Death(i,age),',',panel_r(i,age),',',panel_z(i),',',panel_a(i,age)
 	ENDDO
 	ENDDO
 	CLOSE(UNIT=10)
